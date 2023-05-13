5ca7f9ae44b6c85cc759aa5360cc76743c129116
PrayForPleasures <dmitriyyarchucK@gmail.com>
2023-05-13 17:40:51 +0300

    try to node

--- 01-event-loop-events/index.js
+++ 01-event-loop-events/index.js
@@ -1,41 +1,51 @@
 const fs = require("fs");
 const dns = require("dns");
 
-function timeStamp() {
-	return performance.now().toFixed(2);
+function info(text) {
+	console.log(text, performance.now().toFixed(2));
 }
 
 console.log("start");
 
-// timeouts
-setTimeout(() => {
-	console.log("txt-timeout-1", timeStamp());
-}, 0);
-
-setTimeout(() => {
-	process.nextTick(() => console.log("next-tick-2", timeStamp()));
-	console.log("txt-timeout-2", timeStamp());
-}, 10);
-
 // close events
-fs.writeFile("./test.txt", "hello, NODE.js", () =>
-	console.log("file written", timeStamp())
-);
+fs.writeFile("./test.txt", "hello, NODE.js", () => info("file written"));
 
 // promises
 Promise.resolve().then(() => {
-	console.log("promise 1", timeStamp());
+	info("promise 1");
 });
 
 // next tick
-process.nextTick(() => console.log("next tick 1", timeStamp()));
+process.nextTick(() => info("next tick 1"));
 
 //set immediate
-setImmediate(() => console.log("immediate 1", timeStamp()));
+setImmediate(() => info("immediate 1"));
+
+// timeouts
+setTimeout(() => {
+	info("timeout 1");
+}, 0);
+
+setTimeout(() => {
+	process.nextTick(() => info("next tick 2"));
+	info("timeout 2");
+}, 100);
+
+// intervals
+
+let intervalCount = 0;
+const interval = setInterval(() => {
+	info(`interval ${(intervalCount += 1)}`);
+	if (intervalCount === 2) {
+		clearInterval(interval);
+	}
+}, 50);
 
 // i/o events
 dns.lookup("localhost", (err, address, family) => {
-	console.log("dns 1 localhost", address, timeStamp());
+	info("dns 1 localhost", address);
+	Promise.resolve().then(() => info("promise 2"));
+	process.nextTick(() => info("next tick 3"));
 });
 
 console.log("end");
